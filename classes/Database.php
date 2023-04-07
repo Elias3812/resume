d47f87e4d73404c6c168bc972dc2bb56fb154ca3
Elias3812 <illprt56@gmail.com>
2023-04-07 20:58:11 +0300

    mvc

---
+++ classes/Database.php
@@ -0,0 +1,19 @@
+<?php
+class DataBase {
+    public static $host="localhost";
+    public static $dbName="test";
+    public static $username="root";
+    public static $password="";
+
+        private static function connect() {
+            
+            return mysqli_connect(self::$host,self::$username,self::$password, self::$dbName);
+        }
+
+        public static function query($query, $params = array()) {
+                
+            return (mysqli_query(self::connect(),$query));
+
+        }
+
+}
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php   
+
+spl_autoload_register(function ($class) {
+    if (file_exists('./classes/' . $class . '.php')){
+        require_once './classes/' . $class . '.php';
+    }else if (file_exists('./Controllers/' . $class . '.php')){
+        require_once './Controllers/' . $class . '.php';
+    }
+
+
+   
+});
+require_once('Routes.php');
+
+$root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] ;
+$path = $_SERVER['REQUEST_URI'];
+$p1 = stristr($root.$path, 'resume',true);
+$p2 = stristr($root.$path, 'resume');
+$p2 = stristr($p2,'/',true);
+echo $p1.$p2;
+//echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+
+?> 
