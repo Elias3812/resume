13b8acbeefd263c5667003c833e18ca67e4bde25
Elias3812 <illprt56@gmail.com>
2023-04-08 16:02:18 +0300

    StarSystem

--- Views/Index.php
+++ Views/Index.php
@@ -1,30 +1,32 @@
 <title>Головна</title>
 
 <main>
-    <div class="mainBlock"><img src="https://github.com/Elias3812/resume/blob/5b1ce4a76e92c850687f1d9be876ea1d52fb31ed/img/me.png?raw=true"></div>
+    <div class="mainBlock"><img src="https://github.com/Elias3812/resume/blob/5b1ce4a76e92c850687f1d9be876ea1d52fb31ed/img/me.png?raw=true">
+        <h2>Моє резюме </h2>
+        <h4>Мене звати Пахнюк Ілля Олександрович. Рік народження 2004. Місце проживання Київ вул. Борщагівська 148. Номер телефону 0951454792. Електронна адреса illprt56@gmail.com . </h4>
+   
+
+    </div>
     <div class="mainBlock">
         <h2>Навички</h2>
         <ul>
-            <li><h3><a href="">HTML/CSS3</a></h3></li>
-            <li><h3><a href="">PHP</a></h3></li>
-            <li><h3><a href="">JS</a></h3></li>
-            <li><h3><a href="">MySQL</a></h3></li>
-            <li><h3><a href="">GIT</a></h3></li>
-            <li><h3><a href="">Docker</a></h3></li>
-            <li><h3><a href="">MVC, Rest Api</a></h3></li>
-            <li><h3><a href="">C++</a></h3></li>
-            <li><h3><a href="">Python</a></h3></li>
-            <li><h3><a href="">Linux</a></h3></li>
-            <li><h3><a href="">English Intermediate</a></h3></li>
+            <li><h3><a href="">HTML/CSS3 <br> ⭐⭐⭐⭐⭐</a></h3></li>
+            <li><h3><a href="">PHP <br> ⭐⭐⭐⭐⭐</a></h3></li>
+            <li><h3><a href="">JS <br> ⭐⭐⭐⭐</a></h3></li>
+            <li><h3><a href="">MySQL <br> ⭐⭐⭐⭐</a></h3></li>
+            <li><h3><a href="">GIT <br> ⭐⭐⭐⭐</a></h3></li>
+            <li><h3><a href="">Docker <br> ⭐⭐⭐</a></h3></li>
+            <li><h3><a href="">MVC, Rest Api <br> ⭐⭐⭐⭐</a></h3></li>
+            <li><h3><a href="">C++ <br> ⭐⭐⭐⭐</a></h3></li>
+            <li><h3><a href="">Python <br> ⭐⭐⭐⭐⭐</a></h3></li>
+            <li><h3><a href="">Linux <br> ⭐⭐⭐</a></h3></li>
+            <li><h3><a href="">English Intermediate <br> ⭐⭐⭐</a></h3></li>
 
         </ul>
         
         
     </div>
-    <div class="mainBlock">
-    <h2>Моє резюме </h2>
-        <h4>Мене звати Пахнюк Ілля Олександрович. Рік народження 2004. Місце проживання Київ вул. Борщагівська 148. Номер телефону 0951454792. Електронна адреса illprt56@gmail.com . </h4>
-    </div>
-
+    
+    
 
 </main>
